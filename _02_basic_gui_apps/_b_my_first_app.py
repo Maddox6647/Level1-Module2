cce443e8454f5a2c99b223434a035afe21c1ea01
league <maddoxseul@sbsd.net>
2025-11-28 16:54:08 -0800

    sdgssgs

--- _02_basic_gui_apps/_b_my_first_app.py
+++ _02_basic_gui_apps/_b_my_first_app.py
@@ -10,7 +10,7 @@ def create_image(filename, width, height):
 
     try:
         image = Image.open(filename)
-        image = image.resize((width, height), Image.ANTIALIAS)
+        image = image.resize((width, height), Image.Resampling.LANCZOS)
         image_obj = ImageTk.PhotoImage(image=image)
     except FileNotFoundError as fnf:
         print("ERROR: Unable to find file " + filename)
@@ -21,34 +21,38 @@ def create_image(filename, width, height):
 
 # TODO 1) Make a new class and put tk.Tk in the parenthesis, for example:
 #  FirstApp(tk.TK):
-
+class FirstApp(tk.Tk):
     # TODO 2) Make a constructor
-
+    def __init__(self):
+        super().__init__()
         # TODO 3) Call the Tk class's constructor
         #  super().__init__()
 
         # TODO 4) Add a text label and pick a text message to display
-
+        self.label = tk.Label(self, text="bobby and shrek", bg='yellow',
+                              fg='blue', font=('times new roman', 32, 'bold'), relief='solid')
         # TODO 5) Place the label somewhere on your app. You can use either
         #  x and y or relx and rely
-
+        self.label.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.1)
         # TODO 6) Create a member variable for the image using the
         #  create_image function. You can use the image provided in this
         #  folder or another image from the internet
-
+        self.img = create_image('python.png', 200, 200)
+        self.label_image = tk.Label(self, image=self.img)
         # TODO 7) Create another label and attach to it the image object
         #  from the previous step
-
+        self.label2 = tk.Label(self, text='nick', bg='green',
+                               fg='red', font=('times new roman', 100, 'bold'), relief='solid')
         # TODO 8) Place the label somewhere on your app
-
+        self.label2.place(relx=0.03, rely=0.5, relwidth=0.95, relheight=0.25)
 
 # TODO 9) Create an if __name__ == '__main__': block
-
+if __name__ == '__main__':
     # TODO 10) Create an object of your app class
-
+    first_app = FirstApp()
     # TODO 11) Use the app object to set a title
-
+    first_app.title('FUN FACT: shrek is actually gay with donkey')
     # TODO 12) Use the app object to set the window dimensions (width x height)
-
+    first_app.geometry('450x450')
     # TODO 13) Run the app object's mainloop() method
-
+    first_app.mainloop()
